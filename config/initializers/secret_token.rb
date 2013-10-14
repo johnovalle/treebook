5a6001e3a7c2a66b467ceec462f08ffe0117bfc4
kugyousha <johnovalle@gmail.com>
2013-10-14 18:56:17 -0400

    Update frontend for the new authentication system
    Add simple_form
    Update several forms to bootstrap/simple_form
    Add login/logout/register/profile links to our nav
    Add a collection select to our status form

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Treebook::Application.config.secret_key_base = '5f957ca0d2a57b2922a93c9c70c84407cf946921156a6bab492f4258860f634d513f75e846ed632646deea944d8f4de3c53ab8d49ea5617a535209b4faf19ec4'
+Treebook::Application.config.secret_token = '33556d6265e114f2a48ffc4d185dd9e27b33c4a43fb007bf48f0fbb1c7c2bc41b457aa0e95bd82449e5f85b5f6798690acc9c10e5eb512409cbb670d82d47fc5'

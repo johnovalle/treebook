c4690d390840f4ca6571dfa29b5eff9314fc2fd8
kugyousha <johnovalle@gmail.com>
2013-10-16 09:49:23 -0400

    Styled flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,11 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :profile_name, presence: true,
-                          uniqueness: true,
-                          format: {
-                            with: /a-zA-Z0-9_-/,
-                            message: 'Must be formatted correctly.'
-                          }
+                         uniqueness: true,
+                        format: {
+                          with: /a-zA-Z0-9_-/,
+                          message: 'Must be formatted correctly.'
+                        }
 
   has_many :statuses
 
